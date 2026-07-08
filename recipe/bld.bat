f285d9047dcd3ad7cc68fd227d3cef4db7200b41
gastineau <gastineau@imcce.fr>
2026-07-08 17:46:30 +0200

    [calceph-feedstock] add stdlib

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,3 @@
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DENABLE_FORTRAN=OFF -DENABLE_THREAD=ON -DBUILD_SHARED_LIBS=ON -S . -B build
 cmake --build build --target all 
-cmake --build build --target test 
 cmake --build build --target install
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,5 @@
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_FORTRAN=OFF -DENABLE_THREAD=ON -DBUILD_SHARED_LIBS=ON -S . -B build
 cmake --build build --target all 
-cmake --build build --target test 
 cmake --build build --target install
 rm -fr $PREFIX/libexec
